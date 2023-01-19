b08fc06e538751dd12681cb8ea078d26bf8f8337
b <884387+internetb@users.noreply.github.com>
2023-01-19 11:52:27 -0600

    Update bucket.tf

--- terraform/oracle/bucket.tf
+++ terraform/oracle/bucket.tf
@@ -22,5 +22,21 @@ resource "oci_objectstorage_bucket" "secretsquirrel" {
   }
 }
 
+resource "oci_objectstorage_bucket" "secretsquirrel5" {
+  # bucket can't emit object events
+  # Storage hasn't versioning enabled
+  # Storage isn't encrypted with Customer Managed Key
+  # Object Storage is Public"
+  compartment_id        = oci_identity_compartment.tf-compartment.id
+  name                  = "myreallysecretstore5"
+  namespace             = data.oci_objectstorage_namespace.example.namespace
+  object_events_enabled = false
+  access_type           = "ObjectRead"
+  metadata              = { "data" = "Blockofdata" }
+  storage_tier          = "Standard"
+}
+
+
+
 
 
